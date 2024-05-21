e5cb74ef107db367d9f590db003b4a92e2f8d722
Ahmed Fayad <20812021100138@eng.zu.edu.eg>
2024-05-21 14:41:18 +0300

    remove comments

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ignore_installed = { 'org' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
