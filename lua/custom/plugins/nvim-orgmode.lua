5f738eae7de798e0b235d09ce91c75692cb9639a
Ahmed Fayad <20812021100138@eng.zu.edu.eg>
2024-05-26 21:25:32 +0300

    change the org files

--- lua/custom/plugins/nvim-orgmode.lua
+++ lua/custom/plugins/nvim-orgmode.lua
@@ -7,15 +7,8 @@ return {
       -- Setup orgmode
       require('orgmode').setup {
         org_agenda_files = '~/orgfiles/**/*',
-        org_default_notes_file = '~/orgfiles/refile.org',
+        org_default_notes_file = '~/orgfiles/Todo.org',
       }
-
-      -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
-      -- add `org` to ignore_install
-      -- require('nvim-treesitter.configs').setup({
-      --   ensure_installed = 'all',
-      --   ignore_install = { 'org' },
-      -- })
     end,
   },
 }
